e45cf6001b4e7eb918fb7aff463248ff802e1e61
just a weeb <beol0127@gmail.com>
2021-12-18 14:04:06 +0000

    chore: startup base routes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
 import json
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 
 app = Flask(__name__,
             static_url_path='', 
@@ -10,5 +10,20 @@ app = Flask(__name__,
 @app.route('/')
 def index():
   return render_template("index.html")
+
+@app.route('/anime')
+def anime():
+  return render_template("anime.html")
+
+@app.route('/send_suggestion', methods = ["GET", "POST"])
+def suggestion():
+  if request.method == "GET":
+    return render_template("send.html")
+  if request.method == "POST":
+    value = request.form["anime_name"]
+
+    value = value.jsonify()
+    
+    return render_template("sent.html")
 
 app.run(host = "0.0.0.0", port = 8080)
